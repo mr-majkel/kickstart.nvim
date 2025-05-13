88967d38dff67436f647662700125ec7d5408ecc
Michal Modzelewski <michal.modzelewski@nielseniq.com>
2025-05-13 16:35:40 +0200

    update debug

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -76,6 +76,34 @@ return {
       end,
       desc = 'Debug: See last session result.',
     },
+    {
+      '<leader>dm',
+      function()
+        require('dap').repl.test_method()
+      end,
+      desc = 'Run dap for current test method',
+    },
+    {
+      '<leader>dM',
+      function()
+        require('dap').repl.test_method() { config = { justMyCode = false } }
+      end,
+      desc = 'Run dap for current test method [all code]',
+    },
+    {
+      '<leader>dc',
+      function()
+        require('dap').repl.test_class()
+      end,
+      desc = 'Run dap for current test class',
+    },
+    {
+      '<leader>dC',
+      function()
+        require('dap').repl.test_class() { config = { justMyCode = false } }
+      end,
+      desc = 'Run dap for current test class [all code]',
+    },
   },
   config = function()
     local dap = require 'dap'
@@ -104,17 +132,80 @@ return {
       --    Feel free to remove or use ones that you like more! :)
       --    Don't feel like these are good choices.
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      mappings = {
+        -- Use a table to apply multiple mappings
+        expand = { '<CR>', '<2-LeftMouse>' },
+        open = 'o',
+        remove = 'd',
+        edit = 'e',
+        repl = 'r',
+        toggle = 't',
+      },
+      -- Expand lines larger than the window
+      -- Requires >= 0.7
+      -- expand_lines = vim.fn.has("nvim-0.7") == 1,
+      expand_lines = false,
+      -- Layouts define sections of the screen to place windows.
+      -- The position can be "left", "right", "top" or "bottom".
+      -- The size specifies the height/width depending on position. It can be an Int
+      -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
+      -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
+      -- Elements are the elements shown in the layout (in order).
+      -- Layouts are opened in order so that earlier layouts take priority in window sizing.
       controls = {
+        -- Requires Neovim nightly (or 0.8 when released)
+        enabled = true,
+        -- Display controls in this element
+        element = 'repl',
         icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+          disconnect = '',
+          pause = '',
+          play = '',
+          run_last = '',
+          step_back = '',
+          step_into = '',
+          step_out = '',
+          step_over = '',
+          terminate = '',
+        },
+      },
+      floating = {
+        max_height = nil, -- These can be integers or a float between 0 and 1.
+        max_width = nil, -- Floats will be treated as percentage of your screen.
+        border = 'single', -- Border style. Can be "single", "double" or "rounded"
+        mappings = {
+          close = { 'q', '<Esc>' },
+        },
+      },
+      windows = { indent = 1 },
+      render = {
+        max_type_length = nil, -- Can be integer or nil.
+        max_value_lines = 100, -- Can be integer or nil.
+      },
+      layouts = {
+        {
+          elements = {
+            'repl',
+          },
+          size = 0.30, -- 25% of total lines
+          position = 'top',
+        },
+        {
+          elements = {
+            'scopes',
+            'breakpoints',
+            'stacks',
+          },
+          size = 40, -- 25% of total lines
+          position = 'left',
+        },
+        {
+          elements = {
+            -- Elements can be strings or table with id and size keys.
+            'console',
+          },
+          size = 15, -- 40 columns
+          position = 'bottom',
         },
       },
     }
@@ -137,20 +228,5 @@ return {
 
     require('dap-python').setup()
     require('dap-python').test_runner = 'pytest'
-    local keymap = vim.keymap.set
-    local dap_python = require 'dap-python'
-    keymap('n', '<leader>db', dap.toggle_breakpoint, { silent = true, desc = 'Toggle dap breakpoint' })
-    keymap('n', '<leader>dB', function()
-      dap.toggle_breakpoint(vim.fn.input 'Condition for breakpoint: ')
-    end, { silent = true, desc = 'Toggle dap breakpoint condition' })
-    keymap('n', '<leader>dr', dap.repl.toggle, { silent = true, desc = 'Toggle dap repl' })
-    keymap('n', '<leader>dm', dap_python.test_method, { silent = true, desc = 'Run dap for current test method' })
-    keymap('n', '<leader>dM', function()
-      dap_python.test_method { config = { justMyCode = false } }
-    end, { silent = true, desc = 'Run dap for current test method' })
-    keymap('n', '<leader>dc', dap_python.test_class, { silent = true, desc = 'Run dap for current test class' })
-    keymap('n', '<leader>dC', function()
-      dap_python.test_class { config = { justMyCode = false } }
-    end, { silent = true, desc = 'Run dap for current test class' })
   end,
 }
